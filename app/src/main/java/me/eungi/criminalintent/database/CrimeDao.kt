7df99a79b68ca9ca3c746aa7aa354fcb7619eb78
eungikim <eungikim.me@gmail.com>
2021-08-01 22:17:32 +0900

    Add save changes on detail screen feature

--- app/src/main/java/me/eungi/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/me/eungi/criminalintent/database/CrimeDao.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import me.eungi.criminalintent.Crime
 import java.util.*
 
@@ -14,4 +15,10 @@ interface CrimeDao {
 
     @Query("SELECT * FROM crime WHERE id = :id")
     fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime: Crime)
+
+    @Insert
+    fun addCrime(crime: Crime)
 }
