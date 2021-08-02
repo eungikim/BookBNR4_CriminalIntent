7b20056fae6571ce9c8277186a232731627a7ba3
eungikim <eungikim.me@gmail.com>
2021-08-02 16:22:12 +0900

    Add new crime menu and function

--- app/src/main/java/me/eungi/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/me/eungi/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package me.eungi.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -44,6 +42,11 @@ class CrimeListFragment : Fragment() {
         callbacks = context as Callbacks?
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -72,7 +75,23 @@ class CrimeListFragment : Fragment() {
         super.onDetach()
         callbacks = null
     }
-    
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     private fun updateUI(crimes: List<Crime>) {
 //        adapter = CrimeAdapter(crimes)
